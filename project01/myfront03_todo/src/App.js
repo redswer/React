4df9151239e37204dfffe0a91aa4d05c078f1e43
User -2023BBYXF\Administrator <jin.harry97@gmail.com>
2023-08-11 14:33:39 +0900

    useCallback

--- project01/myfront03_todo/src/App.js
+++ project01/myfront03_todo/src/App.js
@@ -2,7 +2,7 @@ import Header from './components/Header';
 import TodoEditor from './components/TodoEditor';
 import TodoList from './components/TodoList';
 import './App.css';
-import { useReducer, useRef } from 'react';
+import { useReducer, useRef, useCallback } from 'react';
 
 // ** 최적화
 // => 웹서비스의 성능을 개선하는 기술
@@ -37,6 +37,7 @@ import { useReducer, useRef } from 'react';
 //    마운트시의  State 변수값만 사용할 수있으므로 주의해야 하며,
 //    이것을 해결하려면 "함수형 업데이트" 기능을 이용해야함
 //   ( 단, 아래코드처럼 useReducer() 를 이용하는경우에는 무관함 )
+// => 아래의 onUpdate, onDelete 함수를 리랜더링 할때마다 재생성 하지 않도록 적용.
 
 // ** React.memo
 // => 컴포넌트의 불필요한 리랜더링 방지 (Header 에 적용)
@@ -45,7 +46,6 @@ import { useReducer, useRef } from 'react';
 // => 컴포넌트 선언과 동시에 적용하는것 도 가능
 //    const comp = React.memo(() => {....})
 
-// => 아래의 onUpdate, onDelete 함수를 리랜더링 할때마다 재생성 하지 않도록 적용.
 //=================================================
 
 // ** TodoList (일정관리 앱) 3.
@@ -85,48 +85,23 @@ const mockTodo = [{
 
 function reducer(state, action) {
   switch (action.type) {
-
-    // [newItem, ...todo] =>
     case "Create": { return [action.newItem, ...state] }
 
     case "Update": {
       return state.map((it) =>
         it.id === action.targetId ? { ...it, isDone: !it.isDone } : it)
     }
-    /* => const onUpdate = (targetId) => {
-setTodo(todo.map((it) => it.id === targetId ? { ...it, isDone: !it.isDone } : it));
-} 에서 todo.map() = state.map() / targetId = action.targetId */
 
     case "Delete": { return state.filter((it) => it.id !== action.targetId); }
-    /* => const onDelete = (targetId) => {
-        setTodo(todo.filter((it) => it.id !== targetId));
-      } 에서 todo.filter() = state.filter / targetId = action.targetId */
 
-    default:
-      return state;
+    default: return state;
   }
 }
 
 function App() {
-
-  /* const [todo, setTodo] = useState(mockTodo); */
   const [todo, dispatch] = useReducer(reducer, mockTodo);
-  // = > const [State 변수, 상태변화촉발 함수] = useReducer(상태변화함수, 초기값);
-
   const idRef = useRef(mockTodo.length);
 
-  /*
-  const onCreate = (content) => {
-        const newItem = {
-          id: idRef.current,
-          content: content,
-          isDone: false,
-          createDate: new Date().getTime()
-        }
-        setTodo([newItem, ...todo]);
-        idRef.current += 1;
-  }
- */
   const onCreate = (content) => {
     dispatch({
       type: "Create",
@@ -139,37 +114,60 @@ function App() {
     });
     idRef.current += 1;
   }
-  // => 상태값 변경 부분을 reducer() 에게 맡김
-  // => dispatch 호출해서 action 값(type, data) 전달
+
+  console.log('onCreate, todo.length = ' + todo.length);
+
+  // ** useCallback 적용
+  // => 비어있는 배열을 두번째 인자로 전달하여 마운트시에만 생성되도록 함.
+  // => useReducer() 를 이용하므로 State 변수에 직접 접근하지 않기때문에
+  //    "함수형 업데이트" 기능을 사용하지 않아도 됨.
+
+  // => useCallback(callback, [의존성배열])
+  //    [의존성배열] 이 변경되면 callback 함수를 재생성 return 함.
+  // => useCallback(callback, [])
+  //    비어있는 배열을 전달하면 callback 함수는 마운트시에만 생성되고, 이후에는 재생성 되지않음
 
   /*
   const onUpdate = (targetId) => {
-      setTodo(todo.map((it) => it.id === targetId ? { ...it, isDone: !it.isDone } : it));
+      dispatch({
+        type: "Update",
+        targetId
+      });
     }
-  */
-  const onUpdate = (targetId) => {
+    */
+  const onUpdate = useCallback((targetId) => {
     dispatch({
       type: "Update",
       targetId
-      // => targetId: targetId
     });
-  }
-  /*
-  const onDelete = (targetId) => {
-      setTodo(todo.filter((it) => it.id !== targetId));
-    }
-    */
-  const onDelete = (targetId) => {
+
+    console.log('onUpdate_useCallback, todo.length = ' + todo.length);
+    // useCallback 의 첫 번째 인자인 콜백함수는 최초 생성시 전달받은 State변수값(todo.length)만 가지고 있음을 확인
+    // => todo.length 는 todo 배열의 내용(일정)이 추가되어도 초기값인 5로 고정됨
+
+    // 반면에 밑의 주석에서 useCallback 을 적용하지 않은 onDelete에서는 todo.length 가 정상적으로 출력됨
+    // => 일정을 삭제할 때 마다 새로운 state 값을 전달받아 정확한 길이를 출력함(useCallback을 적용하지 않은 onUpdate도 동일)
+  }, []);
+
+  /*   const onDelete = (targetId) => {
+      dispatch({
+        type: "Delete",
+        targetId
+      });
+    console.log('onDelete, todo.length = ' + todo.length);
+    } */
+
+  const onDelete = useCallback((targetId) => {
     dispatch({
       type: "Delete",
       targetId
     });
-  }
+  }, []);
+
+  console.log('** App Update (리랜더링)됨 **');
 
   return (
     <div className='App'>
-      {/* <TestComp /> */}
-
       <Header />
       <TodoEditor onCreate={onCreate} />
       <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete} />
